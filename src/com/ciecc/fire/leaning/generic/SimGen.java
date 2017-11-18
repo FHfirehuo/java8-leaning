349a3b417978c8eb2e4da740b57456321ef870f5
fire <gegeaininiaige@outlook.com>
2017-11-18 13:59:45 +0800

    两个参数或多个

---
+++ src/com/ciecc/fire/leaning/generic/SimGen.java
@@ -0,0 +1,18 @@
+package com.ciecc.fire.leaning.generic;
+
+public class SimGen {
+
+	public static void main(String[] args) {
+		
+		TwoGen<Integer, String> tgObj = new TwoGen<Integer, String>(88, "Generics");
+		
+		tgObj.showTypes();
+		
+		int v = tgObj.getOb1();
+		System.out.println("value: " + v);
+		
+		String str = tgObj.getOb2();
+		System.out.println("value: " + str);
+		
+	}
+}
