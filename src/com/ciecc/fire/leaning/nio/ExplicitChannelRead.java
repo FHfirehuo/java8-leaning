762c85fdf0f87fa1cc76734549c3699b33f740a9
fire <gegeaininiaige@outlook.com>
2017-11-18 22:10:37 +0800

    通过通道读取文件

---
+++ src/com/ciecc/fire/leaning/nio/ExplicitChannelRead.java
@@ -0,0 +1,38 @@
+package com.ciecc.fire.leaning.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class ExplicitChannelRead {
+
+	public static void main(String[] args) {
+		int count = 0;
+		Path filepath = null;
+		
+		
+		try {
+			filepath = Paths.get("test.txt");
+		} catch (Exception e) {
+			System.out.println("path error" + e);
+			return;
+		}
+		
+		try (SeekableByteChannel fByteChannel = Files.newByteChannel(filepath)){
+
+			ByteBuffer mbuf = ByteBuffer.allocate(128);
+			
+			do{
+				
+			}while(count != -1);
+			
+			System.out.println();
+		} catch (Exception e) {
+			System.out.println("I/O error" + e);
+		}
+	}
+
+}
