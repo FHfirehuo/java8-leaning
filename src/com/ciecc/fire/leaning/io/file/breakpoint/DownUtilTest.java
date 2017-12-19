c21ddf216d600d25392283cb9d0a0cbe360b5270
fire <gegeaininiaige@outlook.com>
2017-12-19 18:50:40 +0800

    cc

--- src/com/ciecc/fire/leaning/io/file/breakpoint/DownUtilTest.java
+++ src/com/ciecc/fire/leaning/io/file/breakpoint/DownUtilTest.java
@@ -4,7 +4,8 @@ public class DownUtilTest {
 
 	public static void main(String[] args) throws Exception {
 		final DownUtil downUtil = new DownUtil(
-				"http://apache.mirrors.hoobly.com/tomcat/tomcat-9/v9.0.1/src/apache-tomcat-9.0.1-src.zip",
+				//"http://apache.mirrors.hoobly.com/tomcat/tomcat-9/v9.0.1/src/apache-tomcat-9.0.1-src.zip",
+				"http://mirrors.hust.edu.cn/apache/tomcat/tomcat-9/v9.0.2/bin/apache-tomcat-9.0.2.zip",
 				"apache-tomcat-9.0.1-src.zip", 3);
 
 		downUtil.download();
